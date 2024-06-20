4720feb060b43488df9518b277270254b4527de0
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-21 02:03:53 +0300

    rec

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,6 @@
 <svelte:head>
-    <title> calculator</title>
+    <title>calculator</title>
 </svelte:head>
-    <div class=" bg-red-400 h-[16rem] w-[11rem] rounded-2xl  flex justify-center items-center">
-
-        <div class="bg-green-500 h-[3rem] w-[3rem] rounded-full    ">
-
-        </div>
-        
-    </div>
-
+<div
+    class=" bg-white h-[16rem] w-[11rem] rounded-2xl flex justify-center items-center"
+></div>
