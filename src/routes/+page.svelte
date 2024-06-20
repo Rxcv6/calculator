2ef7632dcfb0db504d77de53c8053d504d9c4b17
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-21 02:52:31 +0300

    white rec

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <svelte:head>
     <title>calculator</title>
 </svelte:head>
-<div class=" bg-white h-[16rem] w-[11rem] rounded-2xl flex justify-center items-center">
+<div class=" bg-red-500 h-[16rem] w-[11rem] rounded-2xl flex justify-center items-center">
     
 </div>
