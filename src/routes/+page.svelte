dc7aa5bbfe36cf8723fb7176342eda4198255612
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-21 01:48:52 +0300

    white rec

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,11 @@
-<!DOCTYPE html>
-<html>
-    <body>
-        <h1>المعلومات الشخصية</h1>
-        <hr>
-        <p>بدر
-            <br><br>
-            
-            مبرمج
-            <br><br>
-            0555032750
-        </p>
-        <h2> السيرة الذاتية </h2>
-        <hr>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam, tempora ipsa? Facere eum eius quo? Quam esse facilis eum harum incidunt. Cumque reprehenderit odit repellat voluptatem quibusdam porro doloribus corrupti.
-        </p>
-        <h2>المشاريع</h2>
-        <hr>
-        <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas mollitia, omnis aperiam dignissimos id earum similique quae voluptatibus ut repellendus impedit voluptatem ducimus dolorum accusantium cupiditate. Quod inventore quidem sint?
-            Eius, incidunt nam voluptates beatae itaque eos corporis asperiores reiciendis accusantium eaque. At delectus facilis fuga obcaecati excepturi. Nisi quis eaque unde distinctio. Eaque necessitatibus quisquam numquam possimus minima reiciendis?
-            Eligendi, in laudantium! Voluptatem nisi, magnam, ea est placeat facere accusamus eaque cum, inventore labore aut maxime? Voluptatem, ipsam! Minima iure voluptate tempora placeat ullam temporibus quod cupiditate quas eveniet!
-            Officia ipsam facere itaque voluptate consequuntur, omnis ducimus distinctio modi minima esse cupiditate iusto voluptatibus dignissimos laudantium vero impedit temporibus fugit perspiciatis, quasi porro quos quod! Harum doloremque veritatis aspernatur?
-            Quibusdam reprehenderit rem similique placeat et architecto dolorem, asperiores error doloremque magni voluptate ea ut, facere aliquam vel, sapiente repudiandae nam soluta. Deleniti dolorum minus magni soluta. Earum, cum numquam.
-            Amet, voluptatem! Doloribus sunt id quas repellendus, hic nostrum neque culpa a eaque eligendi cupiditate accusamus aliquam mollitia fugiat inventore totam dolor perspiciatis voluptatem facilis? Veniam, magnam recusandae! Numquam, alias.
-            Deserunt, sequi optio fuga voluptatem nam quod ad ratione suscipit laborum ipsa excepturi magni dolorem non tempora necessitatibus voluptatum! Quibusdam inventore fuga, ab cupiditate alias voluptate tenetur iste dolorem aliquid.
-            Obcaecati aperiam eos quibusdam natus, dignissimos numquam deleniti dolores placeat voluptas labore dicta similique repellat expedita adipisci molestiae! Esse sint molestias earum assumenda animi commodi id officiis dolor deserunt eius!
-            Ipsam, tempora? Enim porro dolorum beatae assumenda consequuntur. Quod fuga sunt assumenda? Accusamus magni dolorem dignissimos voluptates corporis dolores, aliquid, dolor laboriosam totam nam atque at assumenda, voluptatum vel voluptatibus!
-            Numquam, iste. Quis consequuntur et veritatis recusandae vel officia autem ratione vero cum ipsa obcaecati quam facere architecto est, laborum totam, maiores voluptatibus. Iure aspernatur tempore dolorum quasi, maxime pariatur?
+<svelte:head>
+    <title> calculator</title>
+</svelte:head>
+    <div class=" bg-red-400 h-[16rem] w-[11rem] rounded-2xl  flex justify-center items-center">
 
-        </p>
+        <div class="bg-green-500 h-[3rem] w-[3rem] rounded-full    ">
 
+        </div>
+        
+    </div>
 
-    </body>
-</html>
