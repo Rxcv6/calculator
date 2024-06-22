a6c3517c3a5440635040e26cd2336301d8bddb88
Rxcv6 <161628926+Rxcv6@users.noreply.github.com>
2024-06-22 03:24:14 +0300

    shadows

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
 <svelte:head>
-    <title>calculator</title>
+  <title>calculator</title>
 </svelte:head>
-<div class=" bg-red-500 h-[16rem] w-[11rem] rounded-2xl flex justify-center items-center">
-    
+<div class="grid grid-cols-4 gap-1 p-6 font-semibold bg-white shadow-2xl rounded-2xl ">
+  <div class="flex items-center justify-end h-10 col-span-4 pr-3 mb-1 text-white bg-blue-500 rounded-full ">
+    24543
+  </div>
+  <button id="a">%</button>
+  <button id="a">√</button>
+  <button id="a">CE</button>
+  <button id="w" class="bg-[#1f2b55]">C</button>
+  <button>7</button>
+  <button>8</button>
+  <button>9</button>
+  <button id="w" class="bg-[#fc4557]">-</button>
+  <button>4</button>
+  <button>5</button>
+  <button>6</button>
+  <button id="w" class="bg-[#2784fd]">÷</button>
+  <button>1</button>
+  <button>2</button>
+  <button>3</button>
+  <button id="w" class="bg-[#fdc704]">×</button>
+  <button>.</button>
+  <button>0</button>
+  <button id="a">=</button>
+  <button id="w" class="bg-[#68dc79]">+</button>
 </div>
